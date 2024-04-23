07f22fd944c2b934516e54d4795a138b80ce4e49
Bruno <brunohgrau99@gmail.com>
2024-04-23 20:01:55 -0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import ErrorBlock from "./components/ErrorBlock/ErrorBlock";
-import sushi from "./assets/images/sushi.svg";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-      <ErrorBlock
-        title="This is not food you are looking for"
-        image={<img alt="no restaurants found" src={sushi} />}
-        buttonText="See all restaurants"
-      />
+      <Footer />
       <Outlet />
     </>
   );
